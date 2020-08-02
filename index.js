73be93d1ad36db04265206109dc8e8b308485970
An Yu <ayu3500@gmail.com>
2020-08-02 19:02:03 -0400

    Wrap in try/catch; use single browser tab

--- index.js
+++ index.js
@@ -4,43 +4,49 @@ import fs from 'fs';
 const url='https://www.nytimes.com/crosswords/game/mini';
 
 (async () => {
-  const browser = await puppeteer.launch({ defaultViewport: null });
-  const page = await browser.newPage();
-  await page.setViewport({ width:0, height:0 });
+  try {
+    const browser = await puppeteer.launch({ defaultViewport: null });
+    const [page] = await browser.pages();
 
-  // Wait until no more than 2 active connections open
-  await page.goto(url, {
-    waitUntil: 'networkidle2'
-  });
-  console.log('Page loaded\n');
+    await page.setViewport({ width:0, height:0 });
 
-  await page.waitForSelector('button[aria-label="OK"]', {
-    visible: true,
-  });
-  await page.click('button[aria-label="OK"]');
-  console.log('Modal button clicked\n');
+    // Wait until no more than 2 active connections open
+    await page.goto(url, {
+      waitUntil: 'networkidle2'
+    });
+    console.log('Page loaded\n');
 
-  const element = await page.$('#xwd-board');
-  console.log('Crossword board loaded\n')
+    await page.waitForSelector('button[aria-label="OK"]', {
+      visible: true,
+    });
+    await page.click('button[aria-label="OK"]');
+    console.log('Modal button clicked\n');
 
-  const screenshotPath = 'puzzle.png'
-  await element.screenshot({ path: screenshotPath });
-  console.log(`Screenshot saved to ${screenshotPath}!\n`)
+    const element = await page.$('#xwd-board');
+    console.log('Crossword board loaded\n')
 
-  const clueNumber = 'span[class^="Clue-label"]'
-  const clueElement = 'span[class^="Clue-text"]'
+    const screenshotPath = 'puzzle.png'
+    await element.screenshot({ path: screenshotPath });
+    console.log(`Screenshot saved to ${screenshotPath}!\n`)
 
-  const clueNumberPage = await page.waitForSelector(clueNumber);
-  const cluePage = await page.waitForSelector(clueElement);
-  console.log('Clue text loaded\n')
+    const clueNumber = 'span[class^="Clue-label"]'
+    const clueElement = 'span[class^="Clue-text"]'
 
-  const clueValue = await (await cluePage.getProperty('textContent')).jsonValue();
-  const clueNumValue = await (await clueNumberPage.getProperty('textContent')).jsonValue();
+    const clueNumberHandle = await page.waitForSelector(clueNumber);
+    const clueHandle = await page.waitForSelector(clueElement);
+    console.log('Clue text loaded\n')
 
-  console.log(`Clue ${clueNumValue}: ${clueValue}`)
-  const cluesFile = `${clueNumValue}: ${clueValue}`
+    const clueNumValue = await (await clueNumberHandle.getProperty('textContent')).jsonValue();
+    const clueValue = await (await clueHandle.getProperty('textContent')).jsonValue();
 
-  fs.writeFileSync('clues.txt', cluesFile);
+    console.log(`Clue: ${clueNumValue}: ${clueValue}`)
+    const cluesFile = `${clueNumValue}: ${clueValue}`
 
-  browser.close();
+    fs.writeFileSync('clues.txt', cluesFile);
+
+    browser.close();
+
+  } catch (err) {
+    console.error(err);
+  }
 })();
