238ffef1aa11b50af424e924b42acaccc293e1dc
An Yu <ayu3500@gmail.com>
2020-08-04 17:00:12 -0400

    Include install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#########################################
+# Printer prerequisites
+#########################################
+
+# Install printer system dependencies
+sudo apt-get update
+sudo apt-get install git cups wiringpi build-essential libcups2-dev libcupsimage2-dev python-serial python-pil python-unidecode -y
+
+# # Install printer driver (CUPS filter). Disregard g++ warning.
+git clone https://github.com/adafruit/zj-58
+cd zj-58
+make
+sudo ./install
+
+# # Make printer the default printer
+# # Note the baud rate number may differ depending on printer
+sudo lpadmin -p ZJ-58 -E -v serial:/dev/serial0?baud=19200 -m zjiang/ZJ-58.ppd
+sudo lpoptions -d ZJ-58
+
+# Restart system
+# sudo reboot
+
+# Clone Adafruit Python Thermal Printer library repo
+git clone git@github.com:adafruit/Python-Thermal-Printer.git
+cp Python-Thermal-Printer/Adafruit_Thermal.py ../
+
+# Test printer
+# stty -F /dev/serial0 19200
+# echo -e "This is a test.\\n\\n" > /dev/serial0
+
+#########################################
+# Generally useful
+#########################################
+
+sudo apt-get install vim -y
+
+#########################################
+# NYT mini xword script prerequisites
+#########################################
+
+# Install nvm; node v14+ required
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install v14.4.0
+
+# Needed for Puppeteer
+sudo apt-get install chromium-browser -y
